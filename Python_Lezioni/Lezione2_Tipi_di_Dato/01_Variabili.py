9652fa0a59fa12d30b92c12e9c2191eedd890506
lisamancuso <lisa.mancuso@edu.immaginazioneelavoro.it>
2026-03-04 14:21:55 +0100

    Altri esercizi

--- Python_Lezioni/Lezione2_Tipi_di_Dato/01_Variabili.py
+++ Python_Lezioni/Lezione2_Tipi_di_Dato/01_Variabili.py
@@ -19,4 +19,36 @@ div = y / x
 print("La somma è", somma)
 print("La sottrazione è", sottr)
 print("Il prodotto è", per)
-print("La quoziente è", div)
+print("Il quoziente è", round(div, 2))
+
+# Numeri con la virgola o float
+pi = 3.14159
+temperatura = 21.5
+
+# controllare che tipo sono
+print(type(pi))
+print(type(temperatura))
+
+calcolo = 0.1 + 0.2
+print(calcolo)
+print(round(calcolo, 2)) # arrotondare numeri, numero è per decidere quante cifre dopo la virgola mostrare
+
+#Stringhe: sequenze immutabili di caratteri
+nomeUser = "Anna99"
+emailUser = "anna@mail.com"
+etaUser = 31
+corso = "Tecnico Informatico"
+
+#Concatenazione tra stringhe
+print(nomeUser + " " + emailUser, "anni", etaUser)# + per stringhe e , per numeri
+
+#f-string è il modo moderno di formattare le stringhe
+print(f"{nomeUser} - email: {emailUser} - età {etaUser}")
+
+#ESERCIZIO: presenta te stesso: mioNome, mioCognome, miaEta, mioCorso con il metodo string
+mioNome = "Lisa"
+mioCognome = "Mancuso"
+miaEta = 20
+mioCorso = "Tecnico informatico 2025/26"
+
+print(f"Il mio nome è {mioNome} {mioCognome}, ho {miaEta} anni e frequento il corso di {mioCorso}")
