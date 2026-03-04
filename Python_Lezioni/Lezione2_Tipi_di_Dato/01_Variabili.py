e32c2b28c62a0ad00e6515e7364bc4e419563ce3
lisamancuso <lisa.mancuso@edu.immaginazioneelavoro.it>
2026-03-04 13:49:54 +0100

    Variabili e operazioni

---
+++ Python_Lezioni/Lezione2_Tipi_di_Dato/01_Variabili.py
@@ -0,0 +1,22 @@
+# Questo è un commento
+# Cos'è una variabile? E' un contenitore che memorizza un valore. Questo valore può cambiare (arbitrariamente) durante l'esecuzione del codice.
+
+#Assegno una variabile
+nome = "Lisa"   #String
+eta = 20        #Intero
+corso = "Tecnico informatico 2025/26"   #String
+altezza = 1.64  #Float
+
+#Indipendentemente dal tipo che scelgo non viene dichiarato da nessuna parte. Questo comportamento è tipico dei linguaggi debolmente tipizzati
+
+# ESERCZIO 2: Istanzia 2 variabili (nome a tua scelta) di tipo intero. Con queste 2 variabili elabora le 4 operazioni matematiche. Stampa con print i risultati
+x = 5
+y = 8
+somma = x + y
+sottr = y - x
+per = x * y
+div = y / x
+print("La somma è", somma)
+print("La sottrazione è", sottr)
+print("La moltiplicazione è", per)
+print("La divisione è", div)
