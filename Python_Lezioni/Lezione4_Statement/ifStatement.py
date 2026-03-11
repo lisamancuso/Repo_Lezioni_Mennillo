7efa7f28f46800822d1dae5db130e123fc83eabc
lisamancuso <lisa.mancuso@edu.immaginazioneelavoro.it>
2026-03-11 10:05:46 +0100

    nuova cartella

---
+++ Python_Lezioni/Lezione4_Statement/ifStatement.py
@@ -0,0 +1,10 @@
+# IF STATEMENT. Questo costrutto server a controllare e prendere una scelta.
+# Operatori di confronto
+# == (uguale a)
+# != (diverso da)
+# > (maggiore)
+# < (minore)
+# >= (maggiore uguale)
+# <= (minore uguale)
+
+# Tutte le volte che uso un operatore di confronto sto generando un valore booleano
